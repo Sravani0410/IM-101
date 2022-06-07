0f1a364e4df547f4453f2867e8735e188e487a07
Sravani Sandhya Devi Pyla <sandhyapyla.8@gmail.com>
2022-06-07 20:01:53 +0530

    commit

--- Sprint-2/DAY-4/DEBOUNCER/index.js
+++ Sprint-2/DAY-4/DEBOUNCER/index.js
@@ -1,27 +1,55 @@
 // API:https://www.themealdb.com/api/json/v1/1/search.php?f=a
 
-const getData= async()=>{
+const getData = async() => {
     try{
-      let input=document.getElementsByClassName("input").value
-      console.log(input)
-    }
-    catch(err){
-        console.log(err.message)
+        let input = document.getElementById("input").value
+        console.log('input', input)
+
+        let result = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`);
+        let data = await result.json();
+        console.log('data', data)
+
+        appendData(data.meals)
+
+    } catch(error) {
+        console.log('error', error)
+
     }
 }
 
 
+const callbyfunc = (func, delay) => {
+    let timer;
 
-const appendData=(data)=>{
-    let Div=document.getElementById("display");
-    Div.innerHTML=null;
-    Div.style.display="block";
-    data.map((el)=>{
-        let div=document.createElement("div")
-        div.style.fontSize="20px";
-        div.style.marginTop="10px";
-        div.style.marginBottom="10px";
-        div.innerHTML=el.strMeal;
-        display.append(div);
+    return function () {
+       let context = this;
+        let args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            getData.apply(context, args);
+        }, delay)
+    }
+}
+
+const searchData = callbyfunc(getData, 500);
+
+
+
+const appendData = (data) => {
+    let DisplayDiv = document.getElementById("Display");
+    DisplayDiv.innerHTML = null;
+    DisplayDiv.style.display = "block";
+    console.log(DisplayDiv)
+    data.map((el) => {
+        let Div = document.createElement("div");
+    
+        var image = document.createElement("img");
+         Div.innerHTML = el.strMeal;
+         image.src=el.strMealThumb
+
+         
+        
+
+        DisplayDiv.append(Div,image);
     })
 }
