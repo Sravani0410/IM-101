00d3f2066585408751099bf8bf05a575ccee13f6
Sravani Sandhya Devi Pyla <sandhyapyla.8@gmail.com>
2022-06-08 09:49:15 +0530

    brand update

--- Sprint-5/Day 2/backend part/src/index.js
+++ Sprint-5/Day 2/backend part/src/index.js
@@ -4,9 +4,10 @@ const app=express();
 const port=process.env.PORT || 7878
 app.use(express.json());
 const userController = require("./controllers/usercontrollers");
-
+const brandController=require("./controllers/brandcontrollers")
 
 app.use("/users",userController)
+app.use("/brand",brandController)
 
 
 module.exports={app,port};
