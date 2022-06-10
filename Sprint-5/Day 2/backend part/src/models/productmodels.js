ddfc7e3cc4c3e884ed066562ada27cb05c870eb5
Sravani Sandhya Devi Pyla <sandhyapyla.8@gmail.com>
2022-06-11 01:16:14 +0530

    commit

--- Sprint-5/Day 2/backend part/src/models/productmodels.js
+++ Sprint-5/Day 2/backend part/src/models/productmodels.js
@@ -1,9 +1,8 @@
 const mongoose=require("mongoose")
 
 const productSchema=new mongoose.Schema({
-    category:{type:mongoose.Schema.Types.ObjectId,ref:"category",require:true},
-    options:[
-        {
+    
+    
          name:{type:String,required:true},
          brand:{type:String,required:true},
          image:{type:String,required:true},
@@ -12,9 +11,9 @@ const productSchema=new mongoose.Schema({
          strikedprice:{type:Number,required:true},
          color:{type:String,required:true},
          size:{type:Array,required:false},
-         qty:{type:Number,required:true}
-        }
-    ],
+         qty:{type:Number,required:true},
+         category:{type:Array,required:true}
+     
 }
 ,{
     timestamps:true,
